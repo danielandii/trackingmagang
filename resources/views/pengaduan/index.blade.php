cedd34ea8d0fe9f62463250f1a6a0ff7879ade3b
wahyunovia <wahyunovia36@gmail.com>
2021-04-19 16:30:31 +0700

    view pengaduan

--- resources/views/pengaduan/index.blade.php
+++ resources/views/pengaduan/index.blade.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>FORM PENGADUAN</title>
-
+	
 	<!-- Global stylesheets -->
 	<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
 	<link href="../../../../global_assets/css/icons/icomoon/styles.min.css" rel="stylesheet" type="text/css">
@@ -31,139 +27,25 @@
 <body>
 
 	<!-- Main navbar -->
-	<div class="navbar navbar-expand-md navbar-light">
-
-		<!-- Header with logo -->
+	<div class="navbar navbar-expand-md navbar-dark">
 		<div class="navbar-brand">
 			<a href="../full/index.html" class="d-inline-block">
-				<img src="../../../../global_assets/images/logo_dark.png" alt="">
+				<img src="../../../../global_assets/images/logo_light.png" alt="">
 			</a>
 		</div>
-		<!-- /header with logo -->
-	
 
-		<!-- Mobile controls -->
-		<div class="d-flex d-md-none">
+		<div class="d-md-none">
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-mobile">
 				<i class="icon-tree5"></i>
 			</button>
 		</div>
-		<!-- /mobile controls -->
-
-
-		<!-- Navbar content -->
-		<div class="collapse navbar-collapse" id="navbar-mobile">
-			<ul class="navbar-nav">
-				<li class="nav-item">
-					<a class="navbar-nav-link">
-						<i class="icon-arrow-left8 mr-2"></i>
-						Dashboard
-					</a>
-				</li>
-			</ul>
-		</div>
-		<!-- /navbar content -->
 
 	</div>
 	<!-- /main navbar -->
-
-
-	<!-- Page content -->
-	<div class="page-content">
-
-		<!-- Main content -->
-		<div class="content-wrapper">
-
-			<!-- Page header -->
-			<div class="page-header">
-				<div class="page-header-content header-elements-md-inline">
-					<div class="page-title d-flex">
-						<h4><span class="font-weight-semibold">Form Pengaduan</span></h4>
-						<a href="#" class="header-elements-toggle text-default d-md-none"><i class="icon-more"></i></a>
-					</div>
-				</div>
-			</div>
-			<!-- /page header -->
-
-
-			<!-- Content area -->
-			<div class="content pt-0">
-
-	
-
-
-				<!-- Form layouts -->
-				<div class="row">
-					<div class="col-md-6">
-
-						<!-- Horizontal form -->
-						<div class="card">
-							<div class="card-header header-elements-inline">
-								<h5 class="card-title">Horizontal form</h5>
-								<div class="header-elements">
-									<div class="list-icons">
-				                		<a class="list-icons-item" data-action="collapse"></a>
-				                		<a class="list-icons-item" data-action="reload"></a>
-				                		<a class="list-icons-item" data-action="remove"></a>
-				                	</div>
-			                	</div>
-		                	</div>
-
-							<div class="card-body">
-								<form action="#">
-									<div class="form-group row">
-										<label class="col-lg-3 col-form-label">Text input</label>
-										<div class="col-lg-9">
-											<input type="text" class="form-control">
-										</div>
-									</div>
-
-									<div class="form-group row">
-										<label class="col-lg-3 col-form-label">Password</label>
-										<div class="col-lg-9">
-											<input type="password" class="form-control">
-										</div>
-									</div>
-
-			                        <div class="form-group row">
-			                        	<label class="col-lg-3 col-form-label">Select</label>
-			                        	<div class="col-lg-9">
-				                            <select name="select" class="form-control">
-				                                <option value="opt1">Basic select</option>
-				                                <option value="opt2">Option 2</option>
-				                                <option value="opt3">Option 3</option>
-				                                <option value="opt4">Option 4</option>
-				                                <option value="opt5">Option 5</option>
-				                                <option value="opt6">Option 6</option>
-				                                <option value="opt7">Option 7</option>
-				                                <option value="opt8">Option 8</option>
-				                            </select>
-			                            </div>
-			                        </div>
-
-									<div class="form-group row">
-										<label class="col-lg-3 col-form-label">Textarea</label>
-										<div class="col-lg-9">
-											<textarea rows="5" cols="5" class="form-control" placeholder="Default textarea"></textarea>
-										</div>
-									</div>
-
-									<div class="text-right">
-										<button type="submit" class="btn btn-primary">Submit form <i class="icon-paperplane ml-2"></i></button>
-									</div>
-								</form>
-							</div>
-						</div>
-						<!-- /horizotal form -->
-
-					</div>
-
-					<div class="col-md-6">
-
 						<!-- Vertical form -->
 						<div class="card">
 							<div class="card-header header-elements-inline">
-								<h5 class="card-title">Vertical form</h5>
+								<h5 class="card-title">form pengaduan</h5>
 								<div class="header-elements">
 									<div class="list-icons">
 				                		<a class="list-icons-item" data-action="collapse"></a>
@@ -175,28 +57,22 @@
 
 							<div class="card-body">
 								<form action="#">
+								
+
 									<div class="form-group">
-										<label>Text input</label>
+										<label>email</label>
 										<input type="text" class="form-control">
 									</div>
 
-			                        <div class="form-group">
-			                        	<label>Select</label>
-			                            <select name="select" class="form-control">
-			                                <option value="opt1">Basic select</option>
-			                                <option value="opt2">Option 2</option>
-			                                <option value="opt3">Option 3</option>
-			                                <option value="opt4">Option 4</option>
-			                                <option value="opt5">Option 5</option>
-			                                <option value="opt6">Option 6</option>
-			                                <option value="opt7">Option 7</option>
-			                                <option value="opt8">Option 8</option>
-			                            </select>
-			                        </div>
 
 									<div class="form-group">
 										<label>Textarea</label>
 										<textarea rows="4" cols="4" class="form-control" placeholder="Default textarea"></textarea>
+									</div>
+
+									<div class="form-group">
+										<label>Text input</label>
+										<input type="file" id="alpaca25" class="alpaca-control" name="" autocomplete="off">
 									</div>
 
 									<div class="text-right">
@@ -226,29 +102,9 @@
 
 				<div class="navbar-collapse collapse" id="navbar-footer">
 					<span class="navbar-text">
-						&copy; 2015 - 2018. <a href="#">Limitless Web App Kit</a> by <a href="http://themeforest.net/user/Kopyov" target="_blank">Eugene Kopyov</a>
+						&copy; 2021. 
+						<a href="#"></a> by <a href="http://themeforest.net/user/Kopyov" target="_blank"></a>
 					</span>
-
-					<ul class="navbar-nav ml-lg-auto">
-						<li class="nav-item">
-							<a href="#" class="navbar-nav-link">Text link</a>
-						</li>
-
-						<li class="nav-item">
-							<a href="#" class="navbar-nav-link">
-								<i class="icon-lifebuoy"></i>
-							</a>
-						</li>
-
-						<li class="nav-item">
-							<a href="https://themeforest.net/item/limitless-responsive-web-application-kit/13080328?ref=kopyov" class="navbar-nav-link font-weight-semibold">
-								<span class="text-pink-400">
-									<i class="icon-cart2 mr-2"></i>
-									Purchase
-								</span>
-							</a>
-						</li>
-					</ul>
 				</div>
 			</div>
 			<!-- /footer -->
