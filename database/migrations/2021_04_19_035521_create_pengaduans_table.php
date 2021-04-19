6542e1845bc4650ec593f31e12825a11a1270878
luqmannaufal <luqmannaufal15@gmail.com>
2021-04-19 11:29:16 +0700

    membuat database

---
+++ database/migrations/2021_04_19_035521_create_pengaduans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengaduansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengaduans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('tanggal_pengaduan');
+            $table->string('email');
+            $table->text('laporan_pengaduan');
+            $table->string('file');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengaduans');
+    }
+}
---
+++ database/migrations/2021_04_19_040039_create_tanggapans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTanggapansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tanggapans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->Integer('pengaduan_id');
+            $table->date('tanggal_tanggapan');
+            $table->string('laporan_tanggapan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tanggapans');
+    }
+}
