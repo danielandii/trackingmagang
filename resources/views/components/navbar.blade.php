19d3ffdbd7f4c2b2fb92db5388a957be0a238bae
luqmannaufal <luqmannaufal15@gmail.com>
2021-04-20 14:24:51 +0700

    menghubungkan login

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,10 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-dark " style="background-image:linear-gradient(to right,#1890c8,
-#1aa6e1, #1890c8);">
-<a class="navbar-brand ml-1 p-0" href="/"><img width="140" src="{{asset('img/
-applogo_white.png')}}" alt="logoapp"></a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="
-#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="
-Toggle navigation">
+<nav class="navbar navbar-expand-lg navbar-dark " style="background-image:linear-gradient(to right,#1890c8,#1aa6e1, #1890c8);">
+<a class="navbar-brand ml-1 p-0" href="/"><img width="140" src="{{asset('img/applogo_white.png')}}" alt="logoapp"></a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 <span class="navbar-toggler-icon"></span>
 </button>
 <div class="collapse navbar-collapse " id="navbarnavAltMarkup">
@@ -13,8 +9,7 @@ Toggle navigation">
         border-warning border-top-0 border-left-0 border-right-0 @else active @endif" href="/">
         Home</a>
         <a class="nav-link text-white @if(request()->segment(1) == "lapor") disabled border
-        border-warning border-top-0 border-left-0 border-right-0 @else active @endif" href="/
-        lapor">Buat Pengaduan</a>
+        border-warning border-top-0 border-left-0 border-right-0 @else active @endif" href="/form-pengaduan">Buat Pengaduan</a>
 
         <li class="nav-item dropdown">
             <a href="#" class="nav-link text-white dropdown-toggle active" id="
@@ -32,8 +27,7 @@ Toggle navigation">
             </a> -->
             </div>
         </li>
-        <a class ="nav-link btn text-dark" style="background-image;linear-gradient(to right,gold
-        ,#fff700)"href="/login">Login</a>
+        <a class ="nav-link btn text-dark" style="background-image;linear-gradient(to right,gold,#fff700)"href="/login">Login</a>
     </div>
 </div>
 </nav>
