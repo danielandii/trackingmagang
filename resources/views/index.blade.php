3800e95eb1edc4558edec7ca7a464c16e84e195c
wahyunovia <wahyunovia36@gmail.com>
2021-04-22 11:25:55 +0700

    w

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -168,25 +168,6 @@
 
 
 @endsection
-@extends('layout')
-
-@section('content')
-
-	<!-- Page header -->
-	<div class="page-header page-header-light">
-		<div class="page-header-content header-elements-md-inline">
-			<div class="page-title d-flex">
-				<h4><i class="icon-arrow-left52 mr-2"></i> <span class="font-weight-semibold">Home</span> - Dashboard</h4>
-				<a href="#" class="header-elements-toggle text-default d-md-none"><i class="icon-more"></i></a>
-			</div>
-		</div>
-	</div>
-	<!-- /page header -->
-
-	<!-- Content area -->
-	<div class="content">
-	<div class="content">
-@endsection
 
 @section('js')
 
