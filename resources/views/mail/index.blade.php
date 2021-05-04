ed2849f74fd39b8591dd04a1f4661a5f917c3d2d
wahyunovia <wahyunovia36@gmail.com>
2021-05-04 14:25:07 +0700

    d

--- resources/views/mail/index.blade.php
+++ resources/views/mail/index.blade.php
@@ -1,3 +1,3 @@
-<h1>Halo {{ $no_tiket }}</h1>
-<p>Pengaduan Anda telah kami simpan dengan No Tiket : {{$tanggal_pengaduan}}, Pengaduan anda akan segera kami proses
-atas perhatiannya kami ucapkan terima kasih.</a>.</p>
+<h1>Halo {{$tanggal_pengaduan}}</h1>
+<p>Pengaduan Anda telah kami simpan dengan No Tiket : {{$no_tiket }}, Pengaduan anda akan segera kami proses
+atas perhatiannya kami ucapkan terima kasih.</p>
