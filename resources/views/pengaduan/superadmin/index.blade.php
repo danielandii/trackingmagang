56c07fd68dac7c5effb3f805df4a5318d868e3a6
wahyunovia <wahyunovia36@gmail.com>
2021-05-18 14:23:40 +0700

    cvv

--- resources/views/pengaduan/superadmin/index.blade.php
+++ resources/views/pengaduan/superadmin/index.blade.php
@@ -12,16 +12,9 @@
 	<!-- Content area -->
 	<div class="content">
 		<!-- Hover rows -->
-			<div class="rows">
+			<div class="card">
 				<div class="col">
-					@if ($message = Session::get('success'))
-						<!-- <div class="alert alert-success">
-							{{$message}}
-						</div> -->
-					@endif
-					<div class="table-responsive">
-
-						<table class="table datatable-basic table-hover border">
+					<table class="table datatable-basic table-hover border">
 							<thead>
 								<tr>
 									<th>No</th>
@@ -31,24 +24,17 @@
 									<th>Status Laporan</th>
 									<th class="text-center">Actions</th>
 								</tr>
+							</thead>
+							<tbody>
+								@if(!$dataPengaduan->isEmpty())
+								@php ($i = 1)
                         		@foreach($dataPengaduan as $pengaduan)
                         			<tr>
-										<td>{{ $loop->iteration }}</td>
-										<td>{{ $pengaduan->no_tiket }}</td>
-										<td>{{ $pengaduan->created_at->format('d F Y H:i') }}</td>	
-										<td>{{ $pengaduan->email }}</td>
-										<td>{{ $pengaduan->status }}</td>
-										<!-- {{ route('home-pengaduan.detail', $pengaduan->no_tiket)}} -->
-											<!-- <a href="{{ route('home-pengaduan.detail', $pengaduan->id)}}" class="btn btn-info">
-												<i class="fas fa-clipboard"> Detail</i>
-											</a>
-											<form action="{{ route('home-pengaduan.destroy',$pengaduan->id ) }}" method="post" class="d-inline" onsubmit="return confirm('yakin hapus data')">
-												@method('delete')
-												@csrf 
-												<button class="btn btn-danger btn-sm">
-												<i class="fa fa-trash">delete</i>
-												</button>
-											</form> -->
+										<td>{{$i}}</td>
+										<td><div class="datatable-column-width">{{ $pengaduan->no_tiket }}</div></td>
+										<td><div class="datatable-column-width">{{ $pengaduan->created_at->format('d F Y H:i') }}</div></td>	
+										<td><div class="datatable-column-width">{{ $pengaduan->email }}</div></td>
+										<td><div class="datatable-column-width">{{ $pengaduan->status }}</div></td>
 										<td align="center">
 											<div class="list-icons">
 												<div class="btn-group ml-2 show">
@@ -62,38 +48,43 @@
 													</div>
 												</div>
 											</div>
-											<div id="modal_theme_danger" class="modal fade" tabindex="-1">
-												<div class="modal-dialog">
-													<div class="modal-content">
-														<div class="modal-header bg-danger" align="center">
-															<button type="button" class="close" data-dismiss="modal">&times;</button>
-														</div>
-
-														<form action="{{ route('home-pengaduan.destroy',$pengaduan->id ) }}" method="post" id="delform">
-															@csrf
-															@method('DELETE')
-															<div class="modal-body" align="center">
-																<h2> Hapus Data? </h2>
-															</div>
-
-															<div class="modal-footer">
-																<button type="button" class="btn btn-link" data-dismiss="modal">Batal</button>
-																<button type="submit" class="btn bg-danger">Hapus</button>
-															</div>
-														</form>
-													</div>
-												</div>
-											</div>
-                            			</td>
+										</td>
 									</tr>
+									@php ($i++)
                         		@endforeach
-                        	</thead>
-						</table>
+								@else
+				  				<tr><td align="center" colspan="4">Data Kosong</td></tr>
+								@endif 
+							</tbody>
+                        </table>
 					</div>
 				</div>
-			</div>
 			<!-- /hover rows -->
 		</div>
+		<div id="modal_theme_danger" class="modal fade" tabindex="-1">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header bg-danger" align="center">
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+					</div>
+
+							<form action="{{ route('home-pengaduan.destroy',$pengaduan->id ) }}" method="post" id="delform">
+							@csrf
+							@method('DELETE')
+								<div class="modal-body" align="center">
+									<h2> Hapus Data? </h2>
+								</div>
+
+								<div class="modal-footer">
+									<button type="button" class="btn btn-link" data-dismiss="modal">Batal</button>
+									<button type="submit" class="btn bg-danger">Hapus</button>
+								</div>
+							</form>
+				</div>
+			</div>
+		</div>
+                    			
+									
 	<!-- /content area -->
 
 
@@ -102,19 +93,113 @@
 @section('js')
 
 <!-- Theme JS files -->
-	<script src="{{asset('global_assets/js/plugins/tables/datatables/datatables.min.js')}}"></script>
-	<script src="{{asset('global_assets/js/plugins/notifications/pnotify.min.js')}}"></script>
+<script src="{{asset('global_assets/js/plugins/notifications/pnotify.min.js')}}"></script>
 	<script src="{{asset('global_assets/js/plugins/notifications/bootbox.min.js')}}"></script>
+	<script src="{{asset('global_assets/js/plugins/tables/datatables/datatables.min.js')}}"></script>
 	<script src="{{asset('global_assets/js/plugins/forms/selects/select2.min.js')}}"></script>
-	<script src="{{asset('global_assets/js/plugins/buttons/ladda.min.js')}}"></script>
 	<script src="{{asset('global_assets/js/plugins/buttons/spin.min.js')}}"></script>
+	<script src="{{asset('global_assets/js/plugins/buttons/ladda.min.js')}}"></script>
 
-	<script src="{{asset('global_assets/js/demo_pages/components_modals.js')}}"></script>
 	<script src="{{asset('assets/js/app.js')}}"></script>
-	<script src="assets/js/app.js"></script>
-	
-<!-- /theme JS files -->
-<script type="text/javascript">	
+	<script src="{{asset('global_assets/js/demo_pages/components_modals.js')}}"></script>
+	<script>
+		//modal delete
+		$(document).on("click", ".delbutton", function () {
+		     var url = $(this).data('uri');
+		     $("#delform").attr("action", url);
+		});
+
+		var DatatableBasic = function() {
+
+		    // Basic Datatable examples
+		    var _componentDatatableBasic = function() {
+		        if (!$().DataTable) {
+		            console.warn('Warning - datatables.min.js is not loaded.');
+		            return;
+		        }
+
+		        // Setting datatable defaults
+		        $.extend( $.fn.dataTable.defaults, {
+		            autoWidth: false,
+		            columnDefs: [{ 
+		                orderable: false,
+		                width: 100,
+		                targets: [ 3 ]
+		            }],
+		            dom: '<"datatable-header"fl><"datatable-scroll"t><"datatable-footer"ip>',
+		            language: {
+		                search: '<span>Filter:</span> _INPUT_',
+		                searchPlaceholder: 'Type to filter...',
+		                lengthMenu: '<span>Show:</span> _MENU_',
+		                paginate: { 'first': 'First', 'last': 'Last', 'next': $('html').attr('dir') == 'rtl' ? '&larr;' : '&rarr;', 'previous': $('html').attr('dir') == 'rtl' ? '&rarr;' : '&larr;' }
+		            }
+		        });
+
+		        // Basic datatable
+		        $('.datatable-basic').DataTable();
+
+		        // Alternative pagination
+		        $('.datatable-pagination').DataTable({
+		            pagingType: "simple",
+		            language: {
+		                paginate: {'next': $('html').attr('dir') == 'rtl' ? 'Next &larr;' : 'Next &rarr;', 'previous': $('html').attr('dir') == 'rtl' ? '&rarr; Prev' : '&larr; Prev'}
+		            }
+		        });
+
+		        // Datatable with saving state
+		        $('.datatable-save-state').DataTable({
+		            stateSave: true
+		        });
+
+		        // Scrollable datatable
+		        var table = $('.datatable-scroll-y').DataTable({
+		            autoWidth: true,
+		            scrollY: 300
+		        });
+
+		        // Resize scrollable table when sidebar width changes
+		        $('.sidebar-control').on('click', function() {
+		            table.columns.adjust().draw();
+		        });
+		    };
+
+		    // Select2 for length menu styling
+		    var _componentSelect2 = function() {
+		        if (!$().select2) {
+		            console.warn('Warning - select2.min.js is not loaded.');
+		            return;
+		        }
+
+		        // Initialize
+		        $('.dataTables_length select').select2({
+		            minimumResultsForSearch: Infinity,
+		            dropdownAutoWidth: true,
+		            width: 'auto'
+		        });
+		    };
+
+
+		    //
+		    // Return objects assigned to module
+		    //
+
+		    return {
+		        init: function() {
+		            _componentDatatableBasic();
+		            _componentSelect2();
+		        }
+		    }
+		}();
+
+
+		// Initialize module
+		// ------------------------------
+
+		document.addEventListener('DOMContentLoaded', function() {
+		    DatatableBasic.init();
+		});
+	</script>
+	<script type="text/javascript">
 		$( document ).ready(function() {
 	        // Default style
 	        @if(session('error'))
--- resources/views/tanggapan/superadmin/history.blade.php
+++ resources/views/tanggapan/superadmin/history.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="page-header page-header-light">
+	<div class="page-header page-header-light">
 		<div class="page-header-content header-elements-md-inline">
 			<div class="page-title d-flex">
 				<h4><i class="icon-home2 mr-3 icon-2x"></i> <span class="font-weight-semibold">Home</span> - History Tanggapan</h4>
@@ -14,15 +14,7 @@
 	<div class="content">
 
 		<!-- Hover rows -->
-		<div class="rows">
-			<div class="col">
-			@if ($message = Session::get('success'))
-				<div class="alert alert-success">
-					{{$message}}
-				</div>
-			@endif
-			<div class="table-responsive">
-
+		<div class="card">
 				<table class="table datatable-basic table-hover border">
 					<thead>
 						<tr>
@@ -32,32 +24,35 @@
 							<th>Laporan Tanggapan</th>
 							<th>Status Pengaduan</th>
 							<th class="text-center">Actions</th>
-							
-
 						</tr>
-						@foreach($historytanggapan as $tanggapan)
+					</thead>	
+					<tbody>	
+					@if(!$historytanggapan->isEmpty())
+					@php ($i = 1)
+					@foreach($historytanggapan as $tanggapan)
 							<tr>
-								<td>{{ $loop->iteration }}</td>
-								<td>{{ $tanggapan->pengaduan_tiket}}</td>
-								<td>{{ $tanggapan->created_at->format('d F Y H:i')}}</td>
-								<td>{{ $tanggapan->laporan_tanggapan}}</td>
-								<td>{{ $tanggapan->pengaduan_status}}</td>
+								<td>{{$i}}</td>
+								<td><div class="datatable-column-width">{{ $tanggapan->pengaduan_tiket}}</div></td>
+								<td><div class="datatable-column-width">{{ $tanggapan->created_at->format('d F Y H:i')}}</div></td>
+								<td><div class="datatable-column-width">{{ $tanggapan->laporan_tanggapan}}</div></td>
+								<td><div class="datatable-column-width">{{ $tanggapan->pengaduan_status}}</div></td>
 								<td align="center">
 									<a href="{{ route('home-history.show', $tanggapan->pengaduan_tiket)}}" class="dropdown-item"><i class="icon-file-eye2 mr-0 icon-1x list-icons-item text-blue-300">Show</i></a>
 								</td>
-
-				        </td>	
-
 							</tr>
+						@php ($i++)
                         @endforeach
-					</thead>
+						@else
+				  			<tr><td align="center" colspan="4">Data Kosong</td></tr>
+						@endif
+					</tbody>
 				</table>
 			</div>
-			</div>
 		</div>
+		
 		<!-- /hover rows -->
 
-	</div>
+	
 	<!-- /content area -->
 
 @endsection
@@ -74,6 +69,112 @@
 <script src="{{asset('assets/js/app.js') }}"></script>
 <script src="{{asset('global_assets/js/demo_pages/dashboard.js') }}"></script>
 <!-- /theme JS files -->
+	<script src="{{asset('global_assets/js/plugins/notifications/pnotify.min.js')}}"></script>
+	<script src="{{asset('global_assets/js/plugins/notifications/bootbox.min.js')}}"></script>
+	<script src="{{asset('global_assets/js/plugins/tables/datatables/datatables.min.js')}}"></script>
+	<script src="{{asset('global_assets/js/plugins/forms/selects/select2.min.js')}}"></script>
+	<script src="{{asset('global_assets/js/plugins/buttons/spin.min.js')}}"></script>
+	<script src="{{asset('global_assets/js/plugins/buttons/ladda.min.js')}}"></script>
+
+	<script src="{{asset('assets/js/app.js')}}"></script>
+	<script src="{{asset('global_assets/js/demo_pages/components_modals.js')}}"></script>
+	<script>
+		//modal delete
+		$(document).on("click", ".delbutton", function () {
+		     var url = $(this).data('uri');
+		     $("#delform").attr("action", url);
+		});
+
+		var DatatableBasic = function() {
+
+		    // Basic Datatable examples
+		    var _componentDatatableBasic = function() {
+		        if (!$().DataTable) {
+		            console.warn('Warning - datatables.min.js is not loaded.');
+		            return;
+		        }
+
+		        // Setting datatable defaults
+		        $.extend( $.fn.dataTable.defaults, {
+		            autoWidth: false,
+		            columnDefs: [{ 
+		                orderable: false,
+		                width: 100,
+		                targets: [ 3 ]
+		            }],
+		            dom: '<"datatable-header"fl><"datatable-scroll"t><"datatable-footer"ip>',
+		            language: {
+		                search: '<span>Filter:</span> _INPUT_',
+		                searchPlaceholder: 'Type to filter...',
+		                lengthMenu: '<span>Show:</span> _MENU_',
+		                paginate: { 'first': 'First', 'last': 'Last', 'next': $('html').attr('dir') == 'rtl' ? '&larr;' : '&rarr;', 'previous': $('html').attr('dir') == 'rtl' ? '&rarr;' : '&larr;' }
+		            }
+		        });
+
+		        // Basic datatable
+		        $('.datatable-basic').DataTable();
+
+		        // Alternative pagination
+		        $('.datatable-pagination').DataTable({
+		            pagingType: "simple",
+		            language: {
+		                paginate: {'next': $('html').attr('dir') == 'rtl' ? 'Next &larr;' : 'Next &rarr;', 'previous': $('html').attr('dir') == 'rtl' ? '&rarr; Prev' : '&larr; Prev'}
+		            }
+		        });
+
+		        // Datatable with saving state
+		        $('.datatable-save-state').DataTable({
+		            stateSave: true
+		        });
+
+		        // Scrollable datatable
+		        var table = $('.datatable-scroll-y').DataTable({
+		            autoWidth: true,
+		            scrollY: 300
+		        });
+
+		        // Resize scrollable table when sidebar width changes
+		        $('.sidebar-control').on('click', function() {
+		            table.columns.adjust().draw();
+		        });
+		    };
+
+		    // Select2 for length menu styling
+		    var _componentSelect2 = function() {
+		        if (!$().select2) {
+		            console.warn('Warning - select2.min.js is not loaded.');
+		            return;
+		        }
+
+		        // Initialize
+		        $('.dataTables_length select').select2({
+		            minimumResultsForSearch: Infinity,
+		            dropdownAutoWidth: true,
+		            width: 'auto'
+		        });
+		    };
+
+
+		    //
+		    // Return objects assigned to module
+		    //
+
+		    return {
+		        init: function() {
+		            _componentDatatableBasic();
+		            _componentSelect2();
+		        }
+		    }
+		}();
+
+
+		// Initialize module
+		// ------------------------------
+
+		document.addEventListener('DOMContentLoaded', function() {
+		    DatatableBasic.init();
+		});
+	</script>
 
 @endsection
 
